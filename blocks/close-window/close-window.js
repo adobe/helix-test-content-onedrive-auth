7d300be211e944e8590d019318dc1c8cc0f0cb3e
rofe <github@rofe.com>
2024-02-09 14:05:33 +0100

    chore: close window block

--- blocks/close-window/close-window.js
+++ blocks/close-window/close-window.js
@@ -1,7 +1,6 @@
 export default function decorate(block) {
-    block.innerHTML = `
-    <h1>Login successful</h1>
-    <p>Close this tab to proceed...</p>
-    <script>setTimeout(() => window.close(), 500);</script>
-    `;
+    block.innerHTML = `<h1>Login successful</h1><p>Close this tab to proceed...</p>`;
+    const script = document.createElement('script');
+    script.textContent = 'setTimeout(() => window.close(), 1000);';
+    block.append(script);
 }
